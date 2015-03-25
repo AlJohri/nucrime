8c10fc58524df9b649815315e9d4f25a110ebdff
Al Johri <al.johri@gmail.com>
2015-03-25 17:58:49 -0500

    add tickets to map

--- csv2js.py
+++ csv2js.py
@@ -1,9 +1,17 @@
 import csv, json
 
-data = []
+incidents_data = []
 
 with open("incidents_geocoded.csv", "rU") as f:
 	reader = csv.DictReader(f)
-	for row in reader: data.append(row)
+	for row in reader: incidents_data.append(row)
 
-json.dump(data, open("incidents_geocoded.js", "w"))
+json.dump(incidents_data, open("incidents.js", "w"))
+
+tickets_data = []
+
+with open("tickets_geocoded.csv", "rU") as f:
+	reader = csv.DictReader(f)
+	for row in reader: tickets_data.append(row)
+
+json.dump(tickets_data, open("tickets.js", "w"))
--- geocode.py
+++ geocode.py
@@ -2,20 +2,32 @@ import csv, urllib, requests
 
 geocoded_addresses = {}
 
+def geocode(address):
+    if not geocoded_addresses.get(address):
+        url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+        url += urllib.urlencode({'address': address})
+        data = requests.get(url).json()
+        row['lat'] = data['results'][0]['geometry']['location']['lat']
+        row['lng'] = data['results'][0]['geometry']['location']['lng']
+        geocoded_addresses[address] = (row['lat'], row['lng'])
+    return geocoded_addresses[address]
+
 with open("incidents.csv", "r") as fin, open('incidents_geocoded.csv', 'w') as fout:
     reader = csv.DictReader(fin)
     writer = csv.DictWriter(fout, fieldnames=reader.fieldnames + ['lat', 'lng'])
     writer.writeheader()
     for row in reader:
         address = row['Location'] + ". Evanston, IL 60201"
-        if geocoded_addresses.get(address):
-            row['lat'], row['lng'] = geocoded_addresses[address]
-        else:
-            url = 'https://maps.googleapis.com/maps/api/geocode/json?'
-            url += urllib.urlencode({'address': address})
-            data = requests.get(url).json()
-            row['lat'] = data['results'][0]['geometry']['location']['lat']
-            row['lng'] = data['results'][0]['geometry']['location']['lng']
-            geocoded_addresses[address] = (row['lat'], row['lng'])
+        row['lat'], row['lng'] = geocode(address)
+        print row
+        writer.writerow(row)
+
+with open("tickets.csv", "r") as fin, open("tickets_geocoded.csv", "w") as fout:
+    reader = csv.DictReader(fin)
+    writer = csv.DictWriter(fout, fieldnames=reader.fieldnames + ['lat', 'lng'])
+    writer.writeheader()
+    for row in reader:
+        address = row['Address of Occurrence'] + ". Evanston, IL 60201"
+        row['lat'], row['lng'] = geocode(address)
         print row
         writer.writerow(row)
